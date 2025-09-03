2c6b3c5d098c893edcd852ae2da90714a95105be
Aliahmed0913 <aaosman0913@gmail.com>
2025-09-03 10:49:10 +0300

    'signal_listenTo_userRegistration'

---
+++ users/signals.py
@@ -0,0 +1,18 @@
+from django.db import transaction
+from django.dispatch import receiver
+from django.db.models.signals import post_save
+from users.models import User
+from users.services.verifying_code import VerificationCodeService
+
+@receiver(post_save, sender=User)
+def handle_user_registeration_verify_code(sender, instance, created, **kwargs):
+    '''
+    Listener to user when any registeration event happen.
+    
+    it's create an verify code'''
+    if created :
+        def on_commit():
+            service = VerificationCodeService(instance.id)
+            service.create_code()
+    
+        transaction.on_commit(on_commit)
