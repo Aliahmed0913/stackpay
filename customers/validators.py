9513e49840675aa6c66026f01a0d99699ef46972
Aliahmed0913 <aaosman0913@gmail.com>
2025-09-14 20:33:21 +0300

    addValidatorFor_CustomerAge

--- customers/validators.py
+++ customers/validators.py
@@ -1,9 +1,12 @@
-import phonenumbers 
+import phonenumbers
 from phonenumbers.phonenumberutil import NumberParseException
 from django.core.exceptions import ValidationError
 from django.utils.translation import gettext as _
+from datetime import date
 
-def validate_phone(value):
+ALLOW_AGE = 18
+
+def validate_phone(value:str):
     try:
         phonenumber = phonenumbers.parse(value)
     
@@ -17,3 +20,17 @@ def validate_phone(value):
         raise ValidationError(_('Missing or invalid international format.'))
     
     return str(phonenumbers.format_number(phonenumber,phonenumbers.PhoneNumberFormat.E164))
+
+
+def valid_age(value:date):
+    today = date.today()
+    if value >= today:
+        raise ValidationError(_('Invalid date of birth'))
+   
+    # check if the month,day for today is before birthday month,day return true(1) else false(0)
+    age = today.year - value.year - ((today.month,today.day) < (value.month,value.day))
+    if age >= ALLOW_AGE:          
+        return value
+    raise ValidationError(_('Customer too young'))
+    
+    
