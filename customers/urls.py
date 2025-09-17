d5a9c182263fe10a48fe98091bb7a523ae95d199
Aliahmed0913 <aaosman0913@gmail.com>
2025-09-17 22:00:50 +0300

    config_endpoint_for_customerProfile&addresses

--- customers/urls.py
+++ customers/urls.py
@@ -1,7 +1,14 @@
 
 from django.urls import path, include
-from customers.views import CustomerProfile
+from customers.views import CustomerProfile,CustomerAddress
+from rest_framework.routers import DefaultRouter
+
 app_name = 'customers'
+
+customer_address = DefaultRouter()
+customer_address.register('address',CustomerAddress,basename='customer_address')
+
 urlpatterns = [
     path('profile/',CustomerProfile.as_view()),
+    path('',include(customer_address.urls)),
 ]
