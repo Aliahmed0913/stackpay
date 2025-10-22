dc5f1654f9483ecdc8565afc9b9ac90608aa1448
Aliahmed0913 <aaosman0913@gmail.com>
2025-10-22 11:07:17 +0300

    Auto_set_superuser_to_admin

--- users/models.py
+++ users/models.py
@@ -5,7 +5,6 @@ from django.contrib.auth.models import AbstractUser,UserManager
 class CustomUserManager(UserManager):
     def create_superuser(self, username, email=None, password=None, **extra_fields):
         extra_fields.setdefault("role_management", User.Roles.ADMIN)
-        # extra_fields.setdefault('is_active',True)
         return super().create_superuser(username, email, password, **extra_fields)
     
 class User(AbstractUser):
