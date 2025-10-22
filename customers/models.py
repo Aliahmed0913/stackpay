a65108735e8d71a3434dac8371095f1e60ab49c7
Aliahmed0913 <aaosman0913@gmail.com>
2025-10-22 12:32:55 +0300

    Configure profile&address&kyc tables

--- customers/models.py
+++ customers/models.py
@@ -1,15 +1,22 @@
 from django.db import models
 from django.contrib.auth import get_user_model
 from django_countries.fields import CountryField
+from customers.validators import validate_phone, valid_age, validate_customer_name 
+from rest_framework.exceptions import ValidationError
 # Create your models here.
 User = get_user_model()
 
 class Customer(models.Model):
     user = models.OneToOneField(User,on_delete=models.CASCADE,related_name='customer_profile')
-    full_name = models.CharField(max_length=50, null=True, blank=True)
-    phone_number = models.CharField(max_length=20, unique=True,null=True,blank=True)
-    country = CountryField(default='EG',editable=False)
-    dob = models.DateField(null=True, blank=True)
+    
+    first_name = models.CharField(max_length=50, null=True, blank=True,validators=[validate_customer_name],
+                                help_text='Name must match the name in the document to succeed validation')
+    last_name = models.CharField(max_length=50, null=True, blank=True)
+    phone_number = models.CharField(max_length=20,
+                                    unique=True,
+                                    null=True,blank=True,
+                                    validators=[validate_phone])
+    dob = models.DateField(null=True,blank=True,validators=[valid_age])
     is_verified = models.BooleanField(default=False)
     
     created_at = models.DateTimeField(auto_now_add=True)
@@ -20,13 +27,28 @@ class Customer(models.Model):
   
 class Address(models.Model):
     customer = models.ForeignKey(Customer,on_delete=models.CASCADE,related_name='addresses')
+    country = CountryField(default='EG',editable=False)
     line = models.CharField(max_length=50,blank=True,null=True)
-    state = models.CharField(max_length=20,blank=True,null=True)
-    appartment_number = models.CharField(max_length=20,blank=True,null=True)
-    main_address = models.BooleanField(default=True)
+    city = models.CharField(max_length=50,blank=True,null=True)
+    state = models.CharField(max_length=50,blank=True,null=True)
+    postal_code = models.CharField(max_length=20,blank=True,null=True)
+        
+    building_nubmer = models.CharField(max_length=10,blank=True,null=True)
+    appartment_number = models.CharField(max_length=10,blank=True,null=True)
+    main_address = models.BooleanField(default=False)
     
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    
+    def clean(self):
+        super().clean()
+        if self.country == 'EG' and not (len(self.postal_code) == 5 and (self.postal_code.isdigit())):
+            raise ValidationError({'Postal Code':'In Egypt must be five digits only.'})
+    
+    def save(self, *arg, **kwargs):
+        if self.postal_code:
+            self.full_clean()
+        super().save(*arg, **kwargs)
     
     def __str__(self):
         return f'{self.customer.full_name or self.customer.user.username} - {self.state}'
@@ -38,14 +60,13 @@ class KnowYourCustomer(models.Model):
     
     class Status(models.TextChoices):
         PENDING = 'pending', 'Pending'
-        REVIEW = 'under_review','Under Review'
         APPROVED = 'approved','Approved'
         REJECTED = 'rejected','Rejected'
     
     customer = models.OneToOneField(Customer,on_delete=models.CASCADE,related_name='kyc')
-    document_type = models.CharField(max_length=20, choices=DocumentType.choices,default=DocumentType.NATIONAL_ID)
+    document_type = models.CharField(max_length=20,choices=DocumentType.choices,default=DocumentType.NATIONAL_ID)
     document_id = models.CharField(max_length=100,blank=True,null=True)
-    document_pic = models.FileField(upload_to='kyc-document/')
+    document_file = models.FileField(upload_to='kyc-document/')
     status_tracking = models.CharField(max_length=20,choices=Status.choices,default=Status.PENDING)
     
     created_at = models.DateTimeField(auto_now_add=True)
