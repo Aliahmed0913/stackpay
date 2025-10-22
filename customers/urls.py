1e37cf65e5055a892dd25c048c73d11285e3f50c
Aliahmed0913 <aaosman0913@gmail.com>
2025-10-22 12:21:05 +0300

    Configure customer endpoints

--- customers/urls.py
+++ customers/urls.py
@@ -1,14 +1,15 @@
 
 from django.urls import path, include
-from customers.views import CustomerProfile,CustomerAddress
+from customers.views import CustomerProfileAPIView,CustomerAddressViewSet, KnowYourCustomerAPIView
 from rest_framework.routers import DefaultRouter
 
 app_name = 'customers'
 
 customer_address = DefaultRouter()
-customer_address.register('address',CustomerAddress,basename='customer_address')
+customer_address.register('address',CustomerAddressViewSet,basename='customer_address')
 
-urlpatterns = [
-    path('profile/',CustomerProfile.as_view()),
+urlpatterns =[
     path('',include(customer_address.urls)),
+    path('profile/',CustomerProfileAPIView.as_view()),
+    path('profile/upload-docs/',KnowYourCustomerAPIView.as_view()),
 ]
