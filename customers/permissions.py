f693a200e23ab4bc7651e80f52a081db0709daca
Aliahmed0913 <aaosman0913@gmail.com>
2025-11-19 18:22:01 +0200

    migrations

--- customers/permissions.py
+++
@@ -1,8 +0,0 @@
-from rest_framework.permissions import BasePermission
-from rest_framework.exceptions import PermissionDenied 
-
-class IsCustomerOwnership(BasePermission):
-    message = 'You are not allowed to access this address'
-    def has_object_permission(self, request, view, obj):
-        return request.user.customer_profile == obj.customer
-          
