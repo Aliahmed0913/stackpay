82c34dd90b001960cd4edf9bc2b799f45a984294
Aliahmed0913 <aaosman0913@gmail.com>
2025-12-14 15:35:10 +0200

    remove create transaction-PayMob- orchestration

--- transactions/serializers.py
+++ transactions/serializers.py
@@ -1,7 +1,6 @@
 from rest_framework import serializers
 from transactions.models import Transaction
 from .services.transaction_orchestration import create_transaction
-from .services.paymob import PayMobServiceError
 
 class TransactionSerializer(serializers.ModelSerializer):
     class Meta():
@@ -16,15 +15,5 @@ class TransactionSerializer(serializers.ModelSerializer):
             raise serializers.ValidationError('Invalid amount')
         return value
     
-    def create(self, validated_data):
-        # Customer always derived from request.user.customer_profile
-        request = self.context.get('request')
-        customer = request.user.customer_profile
-        try:
-        
-            transaction = create_transaction(customer,validated_data)
-            return transaction
-        
-        except PayMobServiceError as e:
-            raise serializers.ValidationError({"non_field_errors":[f'{e.details}:{e.message}']})  
+
     
