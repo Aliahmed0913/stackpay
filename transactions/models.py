dc6a0dc1643912bd5bf5356e149f71fa5eb0d139
Aliahmed0913 <aaosman0913@gmail.com>
2025-12-26 10:43:24 +0200

    Support multi-porvider& add transactionState

--- transactions/models.py
+++ transactions/models.py
@@ -5,22 +5,29 @@ from decimal import Decimal
 # Create your models here.
 
 class Transaction(models.Model):
+    class SupportedPaymentProviders(models.TextChoices):
+        PAYMOB = 'PayMob','PayMob'
+
     class TransactionStatus(models.TextChoices):
-        PENDING = 'pending','Pending'
-        SUCCESS = 'success','Success'
+        INITIATED = 'initiated','Initiated'
+        PENDING_PROVIDER = 'pending_provider','Pending Provider'
+        SUCCEEDED = 'succeeded','Succeeded'
         FAILED = 'failed','Failed'
+        REFUNDED = 'refunded','Refunded'    
         
     customer = models.ForeignKey(Customer, on_delete=models.CASCADE, related_name='customer_transaction')
     amount = models.DecimalField(max_digits=12,decimal_places=2,validators=[MinValueValidator(Decimal('0.01'))])
-    status = models.CharField(max_length=10,choices=TransactionStatus.choices,default=TransactionStatus.PENDING)
+    payment_provider = models.CharField(max_length=50,choices=SupportedPaymentProviders.choices,default=SupportedPaymentProviders.PAYMOB)
+    state = models.CharField(max_length=20,editable=False,choices=TransactionStatus.choices,default=TransactionStatus.INITIATED)
     merchant_order_id = models.CharField(max_length=200,unique=True)
-    paymob_order_id = models.CharField(max_length=200,null=True,blank=True)
-    paymob_payment_token = models.TextField(null=True, blank=True)
+    transaction_id = models.CharField(max_length=64,db_index=True,null=True,blank=True)
+    order_id = models.CharField(max_length=200,null=True,blank=True)
+    payment_token = models.TextField(null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    
+
     def __str__(self):
-        return f"Transaction {self.merchant_order_id} ({self.status})"
+        return f"Transaction {self.merchant_order_id} ({self.state})"
 
     class Meta:
         ordering = ['-created_at']
