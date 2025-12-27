b54e42e1878f9a06364d4551a32cc06f93ffd0d2
Aliahmed0913 <aaosman0913@gmail.com>
2025-12-27 20:45:12 +0200

    Use DRF route mechanism for ModelViewSet

--- transactions/urls.py
+++ transactions/urls.py
@@ -1,9 +1,13 @@
-from django.urls import path
-from transactions.views import TransactionAPIView,TransactionView,PayMobWebHookView
+from django.urls import path,include
+from transactions.views import TransactionViewSet,TransactionView,PayMobWebHookView
+from rest_framework.routers import DefaultRouter
+
 app_name = 'transactions'
+register = DefaultRouter()
+register.register(r'transaction',TransactionViewSet,basename='transaction')
 
 urlpatterns = [
+    path('',include(register.urls)),
     path('webhook/',PayMobWebHookView.as_view(),name='transaction-webhook'),
-    path('txn/',TransactionAPIView.as_view(),name='transaction-create'),
     path('testpay-view/',TransactionView.as_view(),name='transaction-view')
 ]
