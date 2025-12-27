e88c83fc33ede312d3f3320a17844334dc37fcd2
Aliahmed0913 <aaosman0913@gmail.com>
2025-12-27 20:40:20 +0200

    Declare top level default function instead of lambda

--- transactions/models.py
+++ transactions/models.py
@@ -5,6 +5,9 @@ from decimal import Decimal
 from customers.models import Customer
 # Create your models here.
 
+def dufault_merchant_order_id():
+    return 'ORD-' + str(uuid.uuid4())
+
 class Transaction(models.Model):
     class SupportedPaymentProviders(models.TextChoices):
         PAYMOB = 'PayMob','PayMob'
@@ -20,7 +23,7 @@ class Transaction(models.Model):
     amount = models.DecimalField(max_digits=12,decimal_places=2,validators=[MinValueValidator(Decimal('0.01'))])
     payment_provider = models.CharField(max_length=50,choices=SupportedPaymentProviders.choices,default=SupportedPaymentProviders.PAYMOB)
     state = models.CharField(max_length=20,editable=False,choices=TransactionState.choices,default=TransactionState.INITIATED)
-    merchant_order_id = models.CharField(max_length=36,unique=True,editable=False,default=lambda:str(uuid.uuid4()))
+    merchant_order_id = models.CharField(max_length=40,unique=True,editable=False,default=dufault_merchant_order_id)
     transaction_id = models.CharField(max_length=64,db_index=True,null=True,blank=True)
     order_id = models.CharField(max_length=200,null=True,blank=True)
     payment_token = models.TextField(null=True, blank=True)
