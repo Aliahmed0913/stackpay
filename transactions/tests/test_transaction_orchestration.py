d8411fa708048fe16964d0f1f157e56f4c92bfe2
Aliahmed0913 <aaosman0913@gmail.com>
2025-12-28 07:18:33 +0200

    Place hint for clarity

--- transactions/tests/test_transaction_orchestration.py
+++ transactions/tests/test_transaction_orchestration.py
@@ -1,16 +1,18 @@
 import pytest
 from transactions.services.transaction_orchestration import create_transaction
+
 @pytest.mark.django_db
 def test_orchestration_transaction(mocker,customer_factory):
     validated_data = {'amount':100.22}
     customer = customer_factory()
+    # replace paymob class with mock to prevent actual calls 
     mock_paymob = mocker.patch('transactions.services.transaction_orchestration.PayMob')
-    mocker.patch('transactions.services.transaction_orchestration.PayMob.generate_id',return_value='ORD-3A2384')
+    # configure mock methods and return values
     instance = mock_paymob.return_value
     instance.create_order.return_value = 'paymob-id2232'
     instance.payment_key_token.return_value = 'test-paymob-token-in-transaction-creation'
-    
+    # call the provider orchestration function
     transaction = create_transaction(customer,validated_data)
     
     assert transaction.customer == customer
-    assert transaction.paymob_payment_token == 'test-paymob-token-in-transaction-creation'
+    assert transaction.payment_token == 'test-paymob-token-in-transaction-creation'
