9c7190cbfb679c8911b2279a4f4df2cb23e0041f
Aliahmed0913 <aaosman0913@gmail.com>
2026-02-01 22:52:04 +0200

    Rename url.names to met standards

--- transactions/urls.py
+++ transactions/urls.py
@@ -1,13 +1,13 @@
-from django.urls import path,include
-from transactions.views import TransactionViewSet,TransactionView,PayMobWebHookView
+from django.urls import path, include
 from rest_framework.routers import DefaultRouter
+from .views import TransactionViewSet, TransactionView, PayMobWebHookView
 
-app_name = 'transactions'
+app_name = "transactions"
 register = DefaultRouter()
-register.register(r'transaction',TransactionViewSet,basename='transaction')
+register.register(r"transaction", TransactionViewSet, basename="transaction")
 
 urlpatterns = [
-    path('',include(register.urls)),
-    path('webhook/',PayMobWebHookView.as_view(),name='transaction-webhook'),
-    path('testpay-view/',TransactionView.as_view(),name='transaction-view')
+    path("", include(register.urls)),
+    path("webhook/", PayMobWebHookView.as_view(), name="transaction_webhook"),
+    path("testpay-view/", TransactionView.as_view(), name="transaction_view"),
 ]
